2fa4e23c470a0237cf14abe4c48e86d2101e71a6
career catalyst <crrcatalyst@gmail.com>
2024-10-20 01:12:35 -0700

    Add files via upload

---
+++ Square.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+
+    int a;
+
+    printf("Enter any number to get it's Square ");
+
+    scanf("%d",&a);
+
+    int b=a*a;
+
+    printf("Square is %d",b);
+
+        return 0; 
+}
