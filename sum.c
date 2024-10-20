ba97589d15613dc87a688d318e5b9ff9d367ab7b
career catalyst <crrcatalyst@gmail.com>
2024-10-20 01:12:01 -0700

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+
+int a;
+int b;
+
+printf("Enter first number ");
+scanf("%d",&a);
+printf("Enter second number ");
+scanf("%d",&b);
+
+
+int c=a+b;
+
+  printf(" sum of a and b is %d",c);
+
+return 0;
+
+}
